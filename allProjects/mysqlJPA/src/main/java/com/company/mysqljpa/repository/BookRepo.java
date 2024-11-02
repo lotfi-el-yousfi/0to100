91de7147deb8dbc8b8e88a23d877e7042fc98a7a
lotfi3003 <51326208+lotfi3003@users.noreply.github.com>
2024-11-02 01:24:04 +0100

    feat: adding actuator and swagger

--- allProjects/mysqlJPA/src/main/java/com/company/mysqljpa/repository/BookRepo.java
+++ allProjects/mysqlJPA/src/main/java/com/company/mysqljpa/repository/BookRepo.java
@@ -2,6 +2,7 @@ package com.company.mysqljpa.repository;
 
 import com.company.mysqljpa.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 public interface BookRepo extends JpaRepository<Book, Long> {
     List<Book> findByAuthor_Name(String authorName);
 
+    @Query("SELECT b FROM Book b WHERE b.author.name = :authorName")
     List<Book> findBooksByAuthorName(@Param("authorName") String authorName);
 
 }
